1a0da404ae15e1c4eec51abf0ee9c77e98b68df8
Raymond <njagala.raymond@gmail.com>
2024-10-23 01:44:52 +0300

    added some tests

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,11 @@
-// src/fibonacci.js
-export function fibonacci(n) {
-    if (n === 0) {
-        return 0;
-    }
+// RAYMOND NJAGALA
+
+// This is a recursive implementation of the fibonacci function 
+function fibonacci(n) {
+    if (n === 0) return 0;  // Base case for 0
+    if (n === 1) return 1;  // Base case for 1
+
+    return fibonacci(n - 1) + fibonacci(n - 2);  
 }
+
+module.exports = fibonacci;
--- test/fibonacci.test.js
+++ test/fibonacci.test.js
@@ -1,9 +1,32 @@
-// test/fibonacci.test.js
-import { expect } from 'chai';
-import { fibonacci } from '../src/fibonacci.js';
+// RAYMOND NJAGALA
+
+
+const { expect } = require('chai');
+const fibonacci = require('../fibonacci');  // Import Fibonacci function
 
 describe('Fibonacci Function', () => {
-    it('should return 0 for fibonacci(0)', () => {
+    // First test: test for the 0th Fibonacci number
+    it('should return 0 when n = 0', () => {
         expect(fibonacci(0)).to.equal(0);
+    });
+
+    // Second test: test for the 1st Fibonacci number
+    it('should return 1 when n = 1', () => {
+        expect(fibonacci(1)).to.equal(1);
+    });
+
+    // Third test: test for a small Fibonacci number
+    it('should return 1 when n = 2', () => {
+        expect(fibonacci(2)).to.equal(1);
+    });
+
+    // Fourth test: evolving test for a larger Fibonacci number
+    it('should return 8 when n = 6', () => {
+        expect(fibonacci(6)).to.equal(8);
+    });
+
+    // Fifth test: evolving test for a bigger Fibonacci number
+    it('should return 21 when n = 8', () => {
+        expect(fibonacci(8)).to.equal(21);
     });
 });
